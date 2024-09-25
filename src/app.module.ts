acdbe0a65fb834199af16edcc4fcfdd38f01a442
Rocketman35 <harhyom9189@gmail.com>
2024-09-25 13:54:35 +0900

    configuration type setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
 import { PostModule } from './post/post.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import configuration, { type NestEnvVariables } from './config/configuration';
 
 @Module({
-  imports: [CatModule, PostModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService<NestEnvVariables>) => ({
+        uri: configService.get('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    CatModule,
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
