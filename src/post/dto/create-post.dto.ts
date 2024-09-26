f4c7001d9f7586f3464dbd03aa525c80838d9a07
RocketMan35 <harhyom9189@gmail.com>
2024-09-26 13:30:14 +0900

    Week1/post/dto-validation (#6)

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,7 +1,16 @@
+import { IsString, IsNotEmpty } from 'class-validator';
 import { Post } from '../schema/post.schema';
 
 export class CreatePostDto implements Post {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   content: string;
+
+  @IsString()
+  @IsNotEmpty()
   author: string;
 }
